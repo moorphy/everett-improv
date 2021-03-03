4981976c2a4de1b827be8dbd5233378ffbf8f9fc
Mike Murphy <murph517@gmail.com>
2021-03-02 17:49:10 -0800

    Intial commit for Everett Improv Wordpress Theme

---
+++ header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+<!-- ********TOP BAR************ -->
+	<div id="top-bar">
+		<div id="contactinfo">
+			<div id = "phone"><i class="fa fa-phone"></i> (425)-259-5912</div>
+			<div id = "address">2804 Grand Ave Everett, WA 98201 Suite 105</div>
+			<div id= "email">everettimprov@gmail.com</div>		
+		</div>
+	</div>
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
+
+		<nav id="main-nav" class="navbar navbar-expand-lg" aria-labelledby="main-nav-label">
+
+			<h2 id="main-nav-label" class="sr-only">
+				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+			</h2>
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<!-- The WordPress Menu goes here -->
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav ml-auto',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'depth'           => 2,
+						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+					)
+				);
+				?>
+
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- #wrapper-navbar end -->
---
+++ page-templates/template-home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: everett homepage
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+get_footer();
